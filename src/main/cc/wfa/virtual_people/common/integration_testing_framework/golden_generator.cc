7706e563737078d1921ed75a321112191a452250
William Stadtlander <66275962+wastadtlander@users.noreply.github.com>
2022-08-16 13:15:12 -0700

    Golden Generator for Integration Testing Framework (#34)
    * Implemented golden generator for integration testing framework
    
    * Implemented files to set up tests for golden_generator
    
    * Added tests for golden generator. Formatting.
    
    * Clang formatting
    
    * Clang formatting
    
    * Potential BUILD formatting fix
    
    * Potential BUILD formatting fix
    
    * Request changes and formatting.
    
    * Refactor to config.proto.
    
    * Clang Formatting
    
    * Requested changes, Formatting

---
+++ src/main/cc/wfa/virtual_people/common/integration_testing_framework/golden_generator.cc
@@ -0,0 +1,61 @@
+// Copyright 2022 The Cross-Media Measurement Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "wfa/virtual_people/common/integration_testing_framework/golden_generator.h"
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "absl/strings/str_cat.h"
+#include "tools/cpp/runfiles/runfiles.h"
+#include "wfa/virtual_people/common/config.pb.h"
+
+using bazel::tools::cpp::runfiles::Runfiles;
+
+namespace wfa_virtual_people {
+
+// Generates CLI commands to generate golden files of binaries based on input
+// config.
+std::vector<std::string> GoldenGenerator(const IntegrationTestList& config) {
+  std::unique_ptr<Runfiles> runfiles(Runfiles::CreateForTest());
+  std::vector<std::string> execute_vector;
+  std::string execute;
+
+  for (int test_index = 0; test_index < config.tests_size(); ++test_index) {
+    const IntegrationTest& it = config.tests().at(test_index);
+    std::string binaryPath = runfiles->Rlocation(it.binary());
+    for (int test_case_index = 0; test_case_index < it.test_cases_size();
+         test_case_index++) {
+      const TestCase& tc = it.test_cases().at(test_case_index);
+      execute = binaryPath;
+      for (int binary_parameter_index = 0;
+           binary_parameter_index < tc.binary_parameters_size();
+           binary_parameter_index++) {
+        BinaryParameter bp = tc.binary_parameters().at(binary_parameter_index);
+        execute = absl::StrCat(execute, " --", bp.name(), "=");
+        if (bp.golden().golden_path().empty()) {
+          absl::StrAppend(&execute, bp.value());
+        } else {
+          absl::StrAppend(&execute, bp.golden().golden_path());
+        }
+      }
+      execute_vector.push_back(execute);
+    }
+  }
+
+  return execute_vector;
+}
+
+}  // namespace wfa_virtual_people
---
+++ src/test/cc/wfa/virtual_people/common/integration_testing_framework/golden_generator_test.cc
@@ -0,0 +1,171 @@
+// Copyright 2022 The Cross-Media Measurement Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "wfa/virtual_people/common/integration_testing_framework/golden_generator.h"
+
+#include <string>
+#include <vector>
+
+#include "gmock/gmock.h"
+#include "google/protobuf/text_format.h"
+#include "gtest/gtest.h"
+#include "wfa/virtual_people/common/config.pb.h"
+
+namespace wfa_virtual_people {
+namespace {
+
+constexpr char bazelOut[] = "bazel-out";
+
+TEST(GoldenGeneratorTest, EmptyConfig) {
+  IntegrationTestList config;
+  std::vector<std::string> execute_vector(GoldenGenerator(config));
+
+  EXPECT_THAT(execute_vector, ::testing::IsEmpty());
+}
+
+TEST(GoldenGeneratorTest, ExpectedConfig) {
+  IntegrationTestList config;
+  ASSERT_TRUE(google::protobuf::TextFormat::ParseFromString(
+      R"pb(
+        tests {
+          name: "model_compiler"
+          binary: "virtual_people_training/src/main/cc/wfa/virtual_people/training/model_compiler/compiler_main"
+          test_cases {
+            name: "population_node"
+            binary_parameters {
+              name: "input_path"
+              value: "src/test/cc/wfa/virtual_people/training/model_compiler/test_data/model_node_config_population_node.textproto"
+            }
+            binary_parameters {
+              name: "output_path"
+              value: "src/test/cc/wfa/virtual_people/training/model_compiler/compiled_node_for_population_node.textproto"
+              golden {
+                golden_path: "src/test/cc/wfa/virtual_people/training/model_compiler/test_data/compiled_node_for_population_node.textproto"
+                proto_path: "virtual_people_common/src/main/proto/wfa/virtual_people/common/_virtual_imports/model_proto/wfa/virtual_people/common/model.proto"
+                proto_dependencies: "virtual_people_common/src/main/proto/wfa/virtual_people/common/_virtual_imports/field_filter_proto/wfa/virtual_people/common/field_filter.proto"
+                proto_type: "CompiledNode"
+              }
+            }
+          }
+          test_cases {
+            name: "population_node_redistribute_probabilities_for_empty_pools"
+            binary_parameters {
+              name: "input_path"
+              value: "src/test/cc/wfa/virtual_people/training/model_compiler/test_data/model_node_config_population_node_redistribute_probabilities_for_empty_pools.textproto"
+            }
+            binary_parameters {
+              name: "output_path"
+              value: "src/test/cc/wfa/virtual_people/training/model_compiler/compiled_node_for_population_node_redistribute_probabilities_for_empty_pools.textproto"
+              golden {
+                golden_path: "src/test/cc/wfa/virtual_people/training/model_compiler/test_data/compiled_node_for_population_node_redistribute_probabilities_for_empty_pools.textproto"
+                proto_path: "virtual_people_common/src/main/proto/wfa/virtual_people/common/_virtual_imports/model_proto/wfa/virtual_people/common/model.proto"
+                proto_dependencies: "virtual_people_common/src/main/proto/wfa/virtual_people/common/_virtual_imports/field_filter_proto/wfa/virtual_people/common/field_filter.proto"
+                proto_type: "CompiledNode"
+              }
+            }
+          }
+          test_cases {
+            name: "population_node_kappa_less_than_one"
+            binary_parameters {
+              name: "input_path"
+              value: "src/test/cc/wfa/virtual_people/training/model_compiler/test_data/model_node_config_population_node_kappa_less_than_one.textproto"
+            }
+            binary_parameters {
+              name: "output_path"
+              value: "src/test/cc/wfa/virtual_people/training/model_compiler/compiled_node_for_population_node_kappa_less_than_one.textproto"
+              golden {
+                golden_path: "src/test/cc/wfa/virtual_people/training/model_compiler/test_data/compiled_node_for_population_node_kappa_less_than_one.textproto"
+                proto_path: "virtual_people_common/src/main/proto/wfa/virtual_people/common/_virtual_imports/model_proto/wfa/virtual_people/common/model.proto"
+                proto_dependencies: "virtual_people_common/src/main/proto/wfa/virtual_people/common/_virtual_imports/field_filter_proto/wfa/virtual_people/common/field_filter.proto"
+                proto_type: "CompiledNode"
+              }
+            }
+          }
+          test_cases {
+            name: "population_node_discretization"
+            binary_parameters {
+              name: "input_path"
+              value: "src/test/cc/wfa/virtual_people/training/model_compiler/test_data/model_node_config_population_node_discretization.textproto"
+            }
+            binary_parameters {
+              name: "output_path"
+              value: "src/test/cc/wfa/virtual_people/training/model_compiler/compiled_node_for_population_node_discretization.textproto"
+              golden {
+                golden_path: "src/test/cc/wfa/virtual_people/training/model_compiler/test_data/compiled_node_for_population_node_discretization.textproto"
+                proto_path: "virtual_people_common/src/main/proto/wfa/virtual_people/common/_virtual_imports/model_proto/wfa/virtual_people/common/model.proto"
+                proto_dependencies: "virtual_people_common/src/main/proto/wfa/virtual_people/common/_virtual_imports/field_filter_proto/wfa/virtual_people/common/field_filter.proto"
+                proto_type: "CompiledNode"
+              }
+            }
+          }
+        }
+      )pb",
+      &config));
+  std::vector<std::string> execute_vector(GoldenGenerator(config));
+
+  for (int i = 0; i < execute_vector.size(); i++) {
+    int index = execute_vector[i].find(bazelOut);
+    execute_vector[i].erase(0, index);
+  }
+
+  std::string element_1 =
+      "bazel-out/k8-fastbuild/bin/src/test/cc/wfa/virtual_people/common/"
+      "integration_testing_framework/golden_generator_test.runfiles/"
+      "virtual_people_training/src/main/cc/wfa/virtual_people/training/"
+      "model_compiler/compiler_main "
+      "--input_path=src/test/cc/wfa/virtual_people/training/model_compiler/"
+      "test_data/model_node_config_population_node.textproto "
+      "--output_path=src/test/cc/wfa/virtual_people/training/model_compiler/"
+      "test_data/compiled_node_for_population_node.textproto";
+  std::string element_2 =
+      "bazel-out/k8-fastbuild/bin/src/test/cc/wfa/virtual_people/common/"
+      "integration_testing_framework/golden_generator_test.runfiles/"
+      "virtual_people_training/src/main/cc/wfa/virtual_people/training/"
+      "model_compiler/compiler_main "
+      "--input_path=src/test/cc/wfa/virtual_people/training/model_compiler/"
+      "test_data/"
+      "model_node_config_population_node_redistribute_probabilities_for_"
+      "empty_pools.textproto "
+      "--output_path=src/test/cc/wfa/virtual_people/training/"
+      "model_compiler/test_data/"
+      "compiled_node_for_population_node_redistribute_probabilities_for_"
+      "empty_pools.textproto";
+  std::string element_3 =
+      "bazel-out/k8-fastbuild/bin/src/test/cc/wfa/virtual_people/common/"
+      "integration_testing_framework/golden_generator_test.runfiles/"
+      "virtual_people_training/src/main/cc/wfa/virtual_people/training/"
+      "model_compiler/compiler_main "
+      "--input_path=src/test/cc/wfa/virtual_people/training/model_compiler/"
+      "test_data/"
+      "model_node_config_population_node_kappa_less_than_one.textproto "
+      "--output_path=src/test/cc/wfa/virtual_people/training/"
+      "model_compiler/test_data/"
+      "compiled_node_for_population_node_kappa_less_than_one.textproto";
+  std::string element_4 =
+      "bazel-out/k8-fastbuild/bin/src/test/cc/wfa/virtual_people/common/"
+      "integration_testing_framework/golden_generator_test.runfiles/"
+      "virtual_people_training/src/main/cc/wfa/virtual_people/training/"
+      "model_compiler/compiler_main "
+      "--input_path=src/test/cc/wfa/virtual_people/training/model_compiler/"
+      "test_data/"
+      "model_node_config_population_node_discretization.textproto "
+      "--output_path=src/test/cc/wfa/virtual_people/training/"
+      "model_compiler/test_data/"
+      "compiled_node_for_population_node_discretization.textproto";
+  EXPECT_THAT(execute_vector, ::testing::ElementsAre(element_1, element_2,
+                                                     element_3, element_4));
+}
+
+}  // namespace
+}  // namespace wfa_virtual_people
